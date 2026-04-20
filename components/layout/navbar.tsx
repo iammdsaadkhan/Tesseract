21b3487300f68ff49d821fe5e070b7a20db2e909
Md Saad Khan <Saadkhan5sep@gmail.com>
2026-04-20 20:50:24 +0530

    sos

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -4,6 +4,8 @@ import { usePathname } from "next/navigation";
 import { navigation } from "@/constants";
 import Button from "../atoms/button";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -11,9 +13,17 @@ const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-50 border-b border-n-6 bg-n-8/90 backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8 font-orbitron font-bold text-2xl text-color-4" href="/">
-          TESSERACT
-        </a>
+        
+        {/* CHANGED: Replaced Text with Logo Image + mix-blend to hide the black background */}
+        <Link className="block w-[12rem] xl:mr-8 flex items-center" href="/">
+          <Image 
+            src="/tesseract-logo.jpg" 
+            alt="Tesseract Logo" 
+            width={200} 
+            height={50} 
+            className="w-auto h-12 object-contain mix-blend-lighten"
+          />
+        </Link>
 
         <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
